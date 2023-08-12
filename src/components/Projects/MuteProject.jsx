826226270e5b4d8306fe3f1591385d547962b7e7
Erik Benedict <89614823+erikbenedict@users.noreply.github.com>
2023-08-11 22:07:36 -0400

    feature: refactored 'MuteProject.jsx' using icon components

--- src/components/Projects/MuteProject.jsx
+++ src/components/Projects/MuteProject.jsx
@@ -1,64 +1,35 @@
 import { Link } from 'react-router-dom';
-import SkillIcon from '../Skills/SkillIcon';
-import jsIcon from '../../assets/images/js.png';
-import htmlIcon from '../../assets/images/html.png';
-import cssIcon from '../../assets/images/css.png';
-import webpackIcon from '../../assets/images/webpack.png';
-import expressIcon from '../../assets/images/express.png';
-import herokuIcon from '../assets/images/heroku.png';
+import {
+  JsIcon,
+  HtmlIcon,
+  CssIcon,
+  WebpackIcon,
+  ExpressIcon,
+  HerokuIcon,
+} from '../Skills/Icons';
 
 export default function MuteProject() {
   return (
     <div className="project-container">
       <h3 className="project-title">M.U.T.E.</h3>
       <div className="project-icons">
-        <SkillIcon
+        <HtmlIcon
           divClassName="project-icon-div"
-          iconSrc={htmlIcon}
-          iconTitle="HTML 5"
-          altText="html icon"
           iconClassName="project-icon"
-          showText={false}
         />
-        <SkillIcon
+        <JsIcon divClassName="project-icon-div" iconClassName="project-icon" />
+        <ExpressIcon
           divClassName="project-icon-div"
-          iconSrc={jsIcon}
-          iconTitle="JavaScript"
-          altText="JavaScript icon"
           iconClassName="project-icon"
-          showText={false}
         />
-        <SkillIcon
+        <CssIcon divClassName="project-icon-div" iconClassName="project-icon" />
+        <WebpackIcon
           divClassName="project-icon-div"
-          iconSrc={expressIcon}
-          iconTitle="Express"
-          altText="express icon"
           iconClassName="project-icon"
-          showText={false}
         />
-        <SkillIcon
+        <HerokuIcon
           divClassName="project-icon-div"
-          iconSrc={cssIcon}
-          iconTitle="CSS 3"
-          altText="css icon"
           iconClassName="project-icon"
-          showText={false}
-        />
-        <SkillIcon
-          divClassName="project-icon-div"
-          iconSrc={webpackIcon}
-          iconTitle="Webpack"
-          altText="webpack icon"
-          iconClassName="project-icon"
-          showText={false}
-        />
-        <SkillIcon
-          divClassName="project-icon-div"
-          iconSrc={herokuIcon}
-          iconTitle="Heroku"
-          altText="heroku icon"
-          iconClassName="project-icon"
-          showText={false}
         />
       </div>
       <p className="project-summary">
